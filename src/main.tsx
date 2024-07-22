b37502ec76caffe7f4907191b4cb6e09b882df2f
irvan <irvan.rahmanto123@gmail.com>
2024-07-22 15:24:15 +0700

    feat: add new pages template

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
-// @tailwind base; 
-// @tailwind components; 
-// @tailwind utilities;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
